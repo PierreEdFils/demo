2a217d717a1fa735fdbbde8b8d99430e8c88eda2
Pierre Fils <epierrefils@gmail.com>
2023-03-31 20:16:35 -0400

    Initial commit

---
+++ src/test/java/practiceapps/com/careerdevs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package practiceapps.com.careerdevs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
